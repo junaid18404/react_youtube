fb5ada46c0006311379bbd2ce912bfdd07b243f9
junaid18404 <junaid_nazir15@yahoo.com>
2020-04-17 15:57:54 +0500

    Modified video item from bootstrap card to youtube like video items.

--- src/components/videoItem.jsx
+++ src/components/videoItem.jsx
@@ -10,22 +10,23 @@ export default function VideoItem(props) {
 
     const imageStyle = {
         width: "100%",
-        height: "30%"
+        height: "20%"
     };
     const cardStyle = {
         cursor: 'pointer',
-        marginBottom: '5%',
+        marginBottom: '2%',
+        marginRight: '2%',
     };
     return (
         <div id='video-item' onClick={() => playVideo()} style={cardStyle}>
-            <div className="card">
-                <div className="card-body">
-                    <img src={thumbnails.high.url} alt="Video Img" style={imageStyle}/>
-                    <div className='text-left'>
-                        <b className="card-title">{title.length > 40 ? `${title.slice(0,50)} ...` : title.slice(0,50)}</b>
-                        <br/>
-                        <small>{DateHelpers.getRelativeFormattedDate(publishedAt)}</small>
-                    </div>
+            <div className="row">
+                <img src={thumbnails.high.url} alt="Video Img" style={imageStyle}/>
+            </div>
+            <div className="row">
+                <div className='text-left'>
+                    <b className="card-title">{title.length > 40 ? `${title.slice(0,50)} ...` : title.slice(0,50)}</b>
+                    <br/>
+                    <small>{DateHelpers.getRelativeFormattedDate(publishedAt)}</small>
                 </div>
             </div>
         </div>
